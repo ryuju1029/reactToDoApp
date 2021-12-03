f3dc6fe558d9bc0ef099f3827c982379639b4350
ryuju1029 <ryuju19971029@gmail.com>
2021-12-03 11:23:10 +0900

    コメント追加

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 
 function TodoList() {
-
   const initialState = [
     {
       task: 'Learn vue.js',
@@ -12,12 +11,12 @@ function TodoList() {
       isCompleted: false
     },
     {
-      task: 'Learn Gatsby.js',
+      task: 'Learn Gatsdy.js',
       isCompleted: false
     },
   ]
 
-  const [todos, setTodo] = useState(initialState);
+  const [todos, SetTodo] = useState(initialState);
   const [task, setTask] = useState('')
   const handleNewTask = (event) => {
     setTask(event.target.value)
@@ -25,36 +24,50 @@ function TodoList() {
   const handleSubmit = (event) => {
     event.preventDefault()
     if (task === '') return
-    setTodo(todos => [...todos, { task, isCompleted: false }])
+    {/*
+     ...todos == スプレッド演算子
+     ...を用いた配列の展開を行うオペレーター
+    */}
+    SetTodo(todos => [...todos, { task, imCompleted: false }])
     setTask('')
   }
-  
+  {/*
+     todosを展開してnewTodosに渡す
+     spliceメソッドを利用してindex番目の要素の1つを削除する
+  */}
+  const handleRemoveTask = index => {
+    const newTodos = [...todos]
+    newTodos.splice(index, 1)
+    SetTodo(newTodos)
+  }
   const handleUpdateTask = index => {
-    let newTodos = todos.map((todo, todoIndex) => {
-      if (todoIndex === index) {
+    let newTodos = todos.map((todo,todoIndex) => {
+      if(todoIndex === index ){
+        {/*
+          isCompletedの値がtureならfalse falseならtureにする
+        */}
         todo.isCompleted = !todo.isCompleted
       }
       return todo;
     })
-    setTodo(newTodos);
-  }
+    SetTodo(newTodos);
+  } 
+
 
   return (
     <div>
       <h1>ToDo List</h1>
       <form onSubmit={handleSubmit}>
-      Add Task :<input value={task} placeholder="Add New Task" onChange={handleNewTask} />
+        add Task : <input value={task} placeholder="Add New Task" onChange={handleNewTask} />
       </form>
+      {/*
+     　　　map関数は配列内の要素をコールバックで処理して、配列としてreturnするメソッド
+     　　　mapの第一引数todosはuseStateでtodosの初期値に指定してあるinitialState配列が入っている
+      */}
       <ul>
         {todos.map((todo, index) => (
-          <li
-            key={index}
-            style={todo.isCompleted === true ? { textDecorationLine: 'line-through' } : {}}
-          >
-            {todo.task}
-            <span onClick={() => handleUpdateTask(index)}>X</span></li>
-        ))
-        }
+          <li key={index} style={ todo.isCompleted === true ? {textDecorationLine: 'line-through'}:{}}>{todo.task}<span onClick={() => handleUpdateTask(index)}>X</span></li>
+        ))}
       </ul>
     </div>
   );
